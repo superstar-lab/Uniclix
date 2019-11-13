ff4ca5623cf5d2b33bfedf9c67855a4a4f365d23
topdev1228 <topdev1228@protonmail.com>
2019-11-13 17:04:39 +0100

    manage posts

--- Client/src/components/Menus/VerticalMenu.js
+++ Client/src/components/Menus/VerticalMenu.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import {NavLink, Link} from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const VerticalMenu = ({ menuItems, channels, selectedChannel, selectChannel }) => {
     return (
-        <div>
-            <aside className="vertical-menu gradient-background-teal-blue scrollbar">
+        <aside className="vertical-menu gradient-background-teal-blue scrollbar">
 
-                <div className="btn-group">
-                    <ProfileInfo selectedChannel = {selectedChannel} />
-                    <ProfileSelectionDropDown channels = {channels} selectChannel={selectChannel} />
-                </div>
-        
-                <MenuItems menuItems={ menuItems } />
-                <SupportSection />
-            </aside>
-        </div>
+            <ProfileInfo selectedChannel={selectedChannel} />
+            <ProfileSelectionDropDown channels={channels} selectChannel={selectChannel} />
+
+            <MenuItems menuItems={menuItems} />
+            <SupportSection />
+        </aside>
     );
 };
 
@@ -22,11 +18,10 @@ const ProfileInfo = ({ selectedChannel }) => (
     <div className="user-dropdown dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <div className="profile-info pull-right">
             <span className="pull-left profile-img-container">
-                <img onError={(e) => e.target.src='/images/dummy_profile.png'} src={selectedChannel.avatar} />
+                <img onError={(e) => e.target.src = '/images/dummy_profile.png'} src={selectedChannel.avatar} />
                 <i className={`fa fa-${selectedChannel.type} ${selectedChannel.type}_bg smallIcon`}></i>
             </span>
             <div className="pull-left">
-                <p className="profile-name">{selectedChannel.name}</p>
                 <p className="profile-username">{!!selectedChannel.username && `@${selectedChannel.username}`}</p>
             </div>
             <div className="pull-right down-arrow">
@@ -40,9 +35,9 @@ const ProfileSelectionDropDown = ({ channels, selectChannel }) => (
     <div className="dropdown-menu select-channel">
         <Link to="/accounts" className="add-channel-btn block-urls">Add new channel</Link>
         <div className="channel-list">
-            {!!channels.length && 
+            {!!channels.length &&
                 channels.map((channel) => (
-                    <ProfileSelectionItem key={channel.id} channel={channel} selectChannel={selectChannel}/>
+                    <ProfileSelectionItem key={channel.id} channel={channel} selectChannel={selectChannel} />
                 ))
             }
         </div>
@@ -54,7 +49,7 @@ const ProfileSelectionItem = ({ channel, selectChannel }) => (
         <a href="#" className="block-urls" onClick={(e) => { selectChannel(channel.id) }}>
             <div className="profile-info pull-right">
                 <span className="pull-left profile-img-container">
-                    <img onError={(e) => e.target.src='/images/dummy_profile.png'} src={channel.avatar} />
+                    <img onError={(e) => e.target.src = '/images/dummy_profile.png'} src={channel.avatar} />
                     <i className={`fa fa-${channel.type} ${channel.type}_bg smallIcon`}></i>
                 </span>
                 <div className="pull-left">
@@ -69,7 +64,7 @@ const ProfileSelectionItem = ({ channel, selectChannel }) => (
 const MenuItems = ({ menuItems }) => (
     <ul className="v-menu-links clear-both">
         {menuItems.map((item) => (
-            <li key={item.id}><NavLink className="links" to={item.uri}>{item.displayName}</NavLink></li>
+            <li key={item.id} ><NavLink className="links" to={item.uri}><i className={`fa fa-${item.icon}`}></i> <span>{item.displayName}</span></NavLink></li>
         ))}
     </ul>
 );
--- Client/src/components/TwitterBooster/Sections/Dashboard.js
+++ Client/src/components/TwitterBooster/Sections/Dashboard.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { connect } from "react-redux";
 import UpgradeAlert from '../../UpgradeAlert';
 import channelSelector from "../../../selectors/channels";
-import {startSetChannels} from "../../../actions/channels";
+import { startSetChannels } from "../../../actions/channels";
 import 'react-dates/initialize';
 import TwitterOverviewCard from '../../Analytics/Twitter/Cards/TwitterOverviewCard';
 import TweetsTable from '../../Analytics/Twitter/Cards/TweetsTable';
+import Tabs from '../../Tabs';
+// require('./styles.css');
 
 class Dashboard extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
@@ -27,15 +29,15 @@ class Dashboard extends React.Component {
     };
 
     setForbidden = (forbidden = false) => {
-        if(forbidden != this.state.forbidden) {
-           this.setState(() => ({
-            forbidden
-           })); 
-        }   
+        if (forbidden != this.state.forbidden) {
+            this.setState(() => ({
+                forbidden
+            }));
+        }
     };
-    
 
-    render(){
+
+    render() {
         const propData = {
             calendarChange: this.state.calendarChange,
             setForbidden: this.setForbidden,
@@ -45,58 +47,74 @@ class Dashboard extends React.Component {
 
         return (
             <div>
-                <h2>DASHBOARD</h2>
+                <div className="section-header no-border">
+                    <div className="section-header__first-row">
+                        <h2>Posts</h2>
+                    </div>
+                </div>
 
-                <UpgradeAlert isOpen={this.state.forbidden && !this.state.loading} goBack={true} setForbidden={this.setForbidden}/>
+                <Tabs>
+                    <div label="Scheduled">
+                        See ya later, <em>Alligator</em>!
+                    </div>
+                    <div label="Awaiting Aproval">
+                        After 'while, <em>Crocodile</em>!
+                        </div>
+                    <div label="Draft">
+                        Nothing to see here, this tab is <em>extinct</em>!
+                    </div>
+                </Tabs>
+
+                <UpgradeAlert isOpen={this.state.forbidden && !this.state.loading} goBack={true} setForbidden={this.setForbidden} />
                 {!this.state.forbidden &&
-                <div>
-                    <div className="row overview-cards-container mb20">
-                        <div className="col-md-3 col-xs-12">
-                            <TwitterOverviewCard
-                                name='Tweets'
-                                type="tweetsCount"
-                                description='tweets'
-                                tooltipDesc='The number of tweets published from your Twitter account'
-                                {...propData} />
+                    <div>
+                        <div className="row overview-cards-container mb20">
+                            <div className="col-md-3 col-xs-12">
+                                <TwitterOverviewCard
+                                    name='Tweets'
+                                    type="tweetsCount"
+                                    description='tweets'
+                                    tooltipDesc='The number of tweets published from your Twitter account'
+                                    {...propData} />
+                            </div>
+                            <div className="col-md-3 col-xs-12">
+                                <TwitterOverviewCard
+                                    name='Followers'
+                                    type="followersCount"
+                                    description='followers'
+                                    tooltipDesc='The number of people who are following your Twitter account'
+                                    {...propData} />
+                            </div>
+                            <div className="col-md-3 col-xs-12">
+                                <TwitterOverviewCard
+                                    name='Following'
+                                    type="followingCount"
+                                    description='following'
+                                    tooltipDesc='Toatal number of people you are following on your Twitter accounts'
+                                    {...propData} />
+                            </div>
+                            <div className="col-md-3 col-xs-12">
+                                <TwitterOverviewCard
+                                    name='Likes'
+                                    type="totalLikesCount"
+                                    description='likes'
+                                    tooltipDesc='Number of likes with your selected account'
+                                    {...propData} />
+                            </div>
                         </div>
-                        <div className="col-md-3 col-xs-12">
-                            <TwitterOverviewCard
-                                name='Followers'
-                                type="followersCount"
-                                description='followers'
-                                tooltipDesc='The number of people who are following your Twitter account'
-                                {...propData} />
-                        </div>
-                        <div className="col-md-3 col-xs-12">
-                            <TwitterOverviewCard
-                                name='Following'
-                                type="followingCount"
-                                description='following'
-                                tooltipDesc='Toatal number of people you are following on your Twitter accounts'
-                                {...propData} />
-                        </div>
-                        <div className="col-md-3 col-xs-12">
-                            <TwitterOverviewCard
-                                name='Likes'
-                                type="totalLikesCount"
-                                description='likes'
-                                tooltipDesc='Number of likes with your selected account'
-                                {...propData} />
-                        </div>
-                    </div>
 
-                    <div className="row mb20">
-                        <div className="col-xs-12">
-                            <TweetsTable
-                                name="Tweets Table"
-                                type='tweetsTableData'
-                                tooltipDesc='The list of tweets published by your Twitter account, with their engagement stats: retweets, replies and likes'
-                                {...propData} />
+                        <div className="row mb20">
+                            <div className="col-xs-12">
+                                <TweetsTable
+                                    name="Tweets Table"
+                                    type='tweetsTableData'
+                                    tooltipDesc='The list of tweets published by your Twitter account, with their engagement stats: retweets, replies and likes'
+                                    {...propData} />
+                            </div>
                         </div>
-                    </div>
 
-                </div> }
-                
+                    </div>}
+
             </div>
         );
     }
