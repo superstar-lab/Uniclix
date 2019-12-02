b4cf2a30511aaafd2f7e3c148376fed528e73598
topdev1228 <topdev1228@protonmail.com>
2019-12-02 13:44:10 +0100

    team modal

--- Client/src/components/Settings/Sections/TeamMember.js
+++ Client/src/components/Settings/Sections/TeamMember.js
@@ -15,7 +15,6 @@ class TeamMember extends React.Component {
 
     render() {
         const { member } = this.props;
-        // console.log('mem', member)
         return (
             <div>
                 {/* {member.assignedChannels[0].active === 1  */}
@@ -60,36 +59,6 @@ class TeamMember extends React.Component {
                 </div>
             </div>
         );
-
-        {/* <div className="clear-both">
-            
-                <div className="item-row">
-
-                    <div>
-                        <div className="profile-info pull-left">
-                        <img className="pull-left" onError={(e) => e.target.src = '/images/dummy_profile.png'} src={channel.avatar} />
-                            <div className="">
-                                <p className="profile-name">{member.details.name}</p>
-                                <span className="profile-username lowercase"> {member.details.email}</span>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div className="item-actions pull-right">
-                    <p className="profile-title">{member.is_admin ? "Admin" : "Member"}</p>
-
-                        <ul className="v-center-align">
-                            <li className="text-links">
-                                <a href="javascript:void(0);" onClick={this.update} className="link-cursor">Edit</a>
-                            </li>
-                            <li className="text-links">
-                                <a href="javascript:void(0);" onClick={this.remove} className="link-cursor">Remove</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div> */}
     }
 }
 
