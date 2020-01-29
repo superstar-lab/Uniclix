69d34c1308d1076dcf0fd7fc62281986eef202d4
topdev1228 <topdev1228@protonmail.com>
2020-01-30 00:40:14 +0200

    Modify stream creator component

--- Client/src/components/TwitterBooster/Sections/StreamCreators.js
+++ Client/src/components/TwitterBooster/Sections/StreamCreators.js
@@ -1,55 +1,54 @@
 import React from 'react';
-import { Typography, Grid, Card, CardContent, Button} from '@material-ui/core';
+import { Typography, Grid, Card, CardContent, Button } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
 class StreamCreators extends React.Component {
     state = {
-        
+
     }
 
-    render(){
-        const {
-            props: {
-              type,
-            },
-          } = this;
-          
-        return(
+    render() {
+        const { creators, onClickCreator } = this.props;
+
+        return (
             <Grid
-				className="dashboard-containers"
+                className="dashboard-containers"
                 container
             >
-                {type.map((kind, key) => (
-                <Grid
-                    container
-                    item
-                    id={key}
-                    lg={3}
-                    md={4}
-                    sm={12}
-                    xs={12}
-                >
-                    <Card className="dashboard-cards">
-                        <CardContent className="dashboard-spacing">
-                            <img src={kind.icon}/>
-                            <Typography className="dashboard-title">
-                                {kind.title}
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                ))}
+                {
+                    creators.map((kind, key) => (
+                        <Grid
+                            container
+                            item
+                            key={key}
+                            lg={3}
+                            md={4}
+                            sm={12}
+                            xs={12}
+                            onClick={() => onClickCreator(kind.title)}
+                        >
+                            <Card className="dashboard-cards">
+                                <CardContent className="dashboard-spacing">
+                                    <img src={kind.icon} />
+                                    <Typography className="dashboard-title">
+                                        {kind.title}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))
+                }
             </Grid>
         );
     }
 }
 
 StreamCreators.defaultProps = {
-    type: [],
+    creators: [],
 }
 
 StreamCreators.propTypes = {
-    type: PropTypes.array.isRequired
+    creators: PropTypes.array.isRequired
 }
 
 export default StreamCreators;
