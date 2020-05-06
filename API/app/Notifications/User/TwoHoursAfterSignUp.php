0b602c09d0a55c31a9b96c35526660069cb2b1c9
topdev1228 <topdev1228@protonmail.com>
2020-05-06 09:25:10 +0300

    Update "two hours after signup" page for mailnotification

---
+++ API/app/Notifications/User/TwoHoursAfterSignUp.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications\User;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class TwoHoursAfterSignUp extends Notification implements ShouldQueue
+{
+    use Queueable;
+    protected $username;
+    public $tries = 3;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $user = $this->username;
+        return (new MailMessage)
+                ->view('emails.user.two_hours_after_signup', [ 'user' => $user ])
+                ->subject('Welcome aboard! Let’s get to it…');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
