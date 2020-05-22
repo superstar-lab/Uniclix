719b7340feb7b9f61771b16dea8731b297fdc4a4
topdev1228 <topdev1228@protonmail.com>
2020-05-22 17:43:40 +0300

    Modify the admin page for autologin.

--- API/app/Http/Controllers/Auth/OAuthController.php
+++ API/app/Http/Controllers/Auth/OAuthController.php
@@ -128,4 +128,25 @@ class OAuthController extends Controller
 
         return response()->json($token);
     }
+
+    protected function autologin(Request $request)
+    {
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $user = User::where('email', $email)->first();
+
+        if(!$user || ($password != $user->password)) return response()->json(["error" => "Incorrect email or password."], 404);
+
+        $user_id = $user->id;
+        // is our way to activate invited users
+        TeamUser::where('member_id', $user_id)->update(['is_pending' => 0]);
+    
+        $token = $user->createToken("Password Token");  
+
+        //The UI needs this value before the portal gets loaded
+        $token->token->accessLevel = $user->getAccessLevel();
+
+        return response()->json($token);
+    }
 }
