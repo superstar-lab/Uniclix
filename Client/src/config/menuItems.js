6577a9bace7df69a61861474d42042d889677422
topdeveloper1228 <topdeveloper1228@gmail.com>
2023-03-14 03:37:23 -0400

    modify: [config] update the config

--- Client/src/config/menuItems.js
+++ Client/src/config/menuItems.js
@@ -1,113 +1,113 @@
 export const membersMenuItems = [
-    {
-        id: 'twitter-booster_manage_posts',
-        displayName: 'Manage posts',
-        uri: '/scheduled',
-        icon: 'pencil'
-    }
+  {
+    id: "twitter-booster_manage_posts",
+    displayName: "Manage posts",
+    uri: "/scheduled",
+    icon: "pencil",
+  },
 ];
 
-export const getMenuItems = (socialMedia) => ([
+export const getMenuItems = (socialMedia) => [
   {
-      id: 'twitter-booster_manage_posts',
-      displayName: 'Manage posts',
-      uri: '/scheduled',
-      icon: 'pencil'
+    id: "twitter-booster_manage_posts",
+    displayName: "Manage posts",
+    uri: "/scheduled",
+    icon: "pencil",
   },
-//   {
-//       id: 'twitter-booster_monitor_activity',
-//       displayName: 'Monitor activity',
-//       uri: '/monitor-activity',
-//       icon: 'desktop'
-//   },
+  //   {
+  //       id: 'twitter-booster_monitor_activity',
+  //       displayName: 'Monitor activity',
+  //       uri: '/monitor-activity',
+  //       icon: 'desktop'
+  //   },
   {
-      id: 'twitter-booster_search',
-      displayName: 'Content Finder',
-      uri: '/content-finder',
-      icon: 'search'
-  },
-  {
-      id: 'analytics',
-      displayName: 'Analytics',
-      uri: `/analytics/${socialMedia}`,
-      icon: 'chart-bar'
+    id: "twitter-booster_search",
+    displayName: "Content Finder",
+    uri: "/content-finder",
+    icon: "search",
   },
   {
-      id: 'twitter-booster_manage_accounts',
-      displayName: 'Accounts',
-      uri: '/settings/manage-account',
-      icon: 'list'
+    id: "analytics",
+    displayName: "Analytics",
+    uri: `/analytics/${socialMedia}`,
+    icon: "chart-bar",
   },
-]);
+  {
+    id: "twitter-booster_manage_accounts",
+    displayName: "Accounts",
+    uri: "/settings/manage-account",
+    icon: "list",
+  },
+];
 
 export const settingsMenus = {
-    'member': [
-        {
-            id: "profile",
-            displayName: "Profile",
-            uri: "/settings/profile",
-            icon: "user"
-        }
-    ],
-    'admin': [
-        {
-            id: "profile",
-            displayName: "Profile",
-            uri: "/settings/profile",
-            icon: "user"
-        },
-        {
-            id: "team",
-            displayName: "Team",
-            uri: "/settings/team",
-            icon: "users"
-        },
-        {
-            id: "manage-account",
-            displayName: "Manage Account",
-            uri: "/settings/manage-account",
-            icon: "list"
-        }
-    ],
-    'owner': [
-        {
-            id: "profile",
-            displayName: "Profile",
-            uri: "/settings/profile",
-            icon: "user"
-        },
-        {
-            id: "team",
-            displayName: "Team",
-            uri: "/settings/team",
-            icon: "users"
-        },
-        {
-            id: "manage-account",
-            displayName: "Manage Account",
-            uri: "/settings/manage-account",
-            icon: "list"
-        },
-        {
-            id: "billing",
-            displayName: "Billing",
-            uri: "/settings/billing",
-            icon: "money-bill-alt"
-        }
-    ]
-};
-
-export const getExtraForSettings = () => ([
+  member: [
     {
-        id: 'twitter-booster_manage_posts',
-        displayName: 'Manage posts',
-        uri: '/scheduled',
-        icon: 'pencil'
+      id: "profile",
+      displayName: "Profile",
+      uri: "/settings/profile",
+      icon: "user",
+    },
+  ],
+  admin: [
+    {
+      id: "profile",
+      displayName: "Profile",
+      uri: "/settings/profile",
+      icon: "user",
     },
     {
-        id: 'twitter-booster_search',
-        displayName: 'Content Finder',
-        uri: '/content-finder',
-        icon: 'search'
-    }
-  ]);
+      id: "team",
+      displayName: "Team",
+      uri: "/settings/team",
+      icon: "users",
+    },
+    {
+      id: "manage-account",
+      displayName: "Manage Account",
+      uri: "/settings/manage-account",
+      icon: "list",
+    },
+  ],
+  owner: [
+    {
+      id: "profile",
+      displayName: "Profile",
+      uri: "/settings/profile",
+      icon: "user",
+    },
+    {
+      id: "team",
+      displayName: "Team",
+      uri: "/settings/team",
+      icon: "users",
+    },
+    {
+      id: "manage-account",
+      displayName: "Manage Account",
+      uri: "/settings/manage-account",
+      icon: "list",
+    },
+    {
+      id: "billing",
+      displayName: "Billing",
+      uri: "/settings/billing",
+      icon: "money-bill-alt",
+    },
+  ],
+};
+
+export const getExtraForSettings = () => [
+  {
+    id: "twitter-booster_manage_posts",
+    displayName: "Manage posts",
+    uri: "/scheduled",
+    icon: "pencil",
+  },
+  {
+    id: "twitter-booster_search",
+    displayName: "Content Finder",
+    uri: "/content-finder",
+    icon: "search",
+  },
+];
--- Client/src/config/routesPerRole.js
+++ Client/src/config/routesPerRole.js
@@ -1,40 +1,40 @@
 const MEMBER_ROUTES = [
-  '/',
-  '/accounts',
-  '/scheduled',
-  '/scheduled/posts',
-  '/settings',
-  '/settings/profile'
+  "/",
+  "/accounts",
+  "/scheduled",
+  "/scheduled/posts",
+  "/settings",
+  "/settings/profile",
 ];
 
 const ADMIN_ROUTES = [
   ...MEMBER_ROUTES,
-  '/scheduled/unapproved',
-  '/scheduled/past',
+  "/scheduled/unapproved",
+  "/scheduled/past",
   // '/monitor-activity',
-  '/content-finder',
-  '/analytics/facebook',
-  '/analytics/twitter',
-  '/analytics/linkedin',
-  '/settings/manage-account',
-  '/settings',
-  '/settings/team',
+  "/content-finder",
+  "/analytics/facebook",
+  "/analytics/twitter",
+  "/analytics/linkedin",
+  "/settings/manage-account",
+  "/settings",
+  "/settings/team",
 ];
 
 const OWNER_ROUTES = [
   ...ADMIN_ROUTES,
-  '/settings/billing',
-  '/settings/billing/plans',
-  '/settings/billing/thank-you-basic',
-  '/settings/billing/thank-you-premium',
-  '/settings/billing/thank-you-pro',
-  '/on-boarding'
+  "/settings/billing",
+  "/settings/billing/plans",
+  "/settings/billing/thank-you-basic",
+  "/settings/billing/thank-you-premium",
+  "/settings/billing/thank-you-pro",
+  "/on-boarding",
 ];
 
 const ROUTES = {
-  'member': MEMBER_ROUTES,
-  'admin': ADMIN_ROUTES,
-  'owner': OWNER_ROUTES
-}
+  member: MEMBER_ROUTES,
+  admin: ADMIN_ROUTES,
+  owner: OWNER_ROUTES,
+};
 
 export default ROUTES;
