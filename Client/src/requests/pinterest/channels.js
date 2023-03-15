aa4e91b5d043c552f7fc7154118bb73eb663929d
topdeveloper1228 <topdeveloper1228@gmail.com>
2023-03-15 10:49:49 -0400

    modify: [requests] update the requests

--- Client/src/requests/pinterest/channels.js
+++ Client/src/requests/pinterest/channels.js
@@ -1,16 +1,20 @@
 import axios from "axios";
-import {apiUrl} from "../../config/api";
+import { apiUrl } from "../../config/api";
 
 export const addChannel = (accessToken) => {
-    return axios.post(`${apiUrl}/pinterest/channels/add`, {
-                access_token: accessToken,
-            }).then((response) => {
-                return response.data;
-            });
+  return axios
+    .post(`${apiUrl}/pinterest/channels/add`, {
+      access_token: accessToken,
+    })
+    .then((response) => {
+      return response.data;
+    });
 };
 
 export const getBoards = (id) => {
-    return axios.get(`${apiUrl}/pinterest/channels/boards?id=${id}`).then((response) => {
-                return response.data;
-            });
+  return axios
+    .get(`${apiUrl}/pinterest/channels/boards?id=${id}`)
+    .then((response) => {
+      return response.data;
+    });
 };
