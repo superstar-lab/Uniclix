c01e1a1dc0fe4a139f15ce9bd884dc40f528b733
topdeveloper1228 <topdeveloper1228@gmail.com>
2023-03-16 03:55:38 -0400

    modify: [store] update the store

--- Client/src/store/configStore.js
+++ Client/src/store/configStore.js
@@ -1,4 +1,4 @@
-import {createStore, combineReducers, applyMiddleware, compose} from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import authReducer from "../reducers/auth";
 import channelReducer from "../reducers/channels";
@@ -6,22 +6,22 @@ import postReducer from "../reducers/posts";
 import composerReducer from "../reducers/composer";
 import profileReducer from "../reducers/profile";
 import middlewareReducer from "../reducers/middleware";
-import generalReducer from '../reducers/general';
+import generalReducer from "../reducers/general";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default () => {
-    const store = createStore(
-        combineReducers({
-            auth: authReducer,
-            middleware: middlewareReducer,
-            profile: profileReducer,
-            channels: channelReducer,
-            posts: postReducer,
-            composer: composerReducer,
-            general: generalReducer
-        }),
-        composeEnhancers(applyMiddleware(thunk))
-    );
+  const store = createStore(
+    combineReducers({
+      auth: authReducer,
+      middleware: middlewareReducer,
+      profile: profileReducer,
+      channels: channelReducer,
+      posts: postReducer,
+      composer: composerReducer,
+      general: generalReducer,
+    }),
+    composeEnhancers(applyMiddleware(thunk))
+  );
 
-    return store;
-}
+  return store;
+};
