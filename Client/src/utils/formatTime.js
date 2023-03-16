055a53ed578e62896731f44fb56438d31d5c3816
topdeveloper1228 <topdeveloper1228@gmail.com>
2023-03-16 04:02:56 -0400

    modify: [utils] update the utils

--- Client/src/utils/formatTime.js
+++ Client/src/utils/formatTime.js
@@ -1,48 +1,46 @@
-import moment from 'moment';
-import { isSafari } from '../utils/helpers';
+import moment from "moment";
+import { isSafari } from "../utils/helpers";
 
-function clearNumber(value = '') {
-    return value.replace(/\D+/g, '')
+function clearNumber(value = "") {
+  return value.replace(/\D+/g, "");
 }
 
 export default (value) => {
+  const clearValue = clearNumber(value);
+  let hour = clearValue.slice(0, 2);
+  let minutes = clearValue.slice(2, 4);
+  if (clearValue.slice(0, 1) > 1) {
+    hour = `0${clearValue.slice(0, 1)}`;
+  }
+  if (clearValue.slice(0, 2) > 12) {
+    hour = `0${clearValue.slice(0, 1)}`;
+  }
+  if (clearValue.slice(2, 3) > 5) {
+    minutes = `0${clearValue.slice(2, 3)}`;
+  }
 
-    const clearValue = clearNumber(value)
-    let hour = clearValue.slice(0, 2)
-    let minutes = clearValue.slice(2, 4)
-    if (clearValue.slice(0, 1) > 1) {
-        hour = `0${clearValue.slice(0, 1)}`
-    }
-    if (clearValue.slice(0, 2) > 12) {
-        hour = `0${clearValue.slice(0, 1)}`
-    }
-    if (clearValue.slice(2, 3) > 5) {
-        minutes = `0${clearValue.slice(2, 3)}`
-    }
-
-    if (clearValue.length >= 3) {
-        return `${hour}:${minutes}`
-    } else {
-        return hour
-    }
-
-}
+  if (clearValue.length >= 3) {
+    return `${hour}:${minutes}`;
+  } else {
+    return hour;
+  }
+};
 
 // This help us to create a date object maintaining the timezon configured
 // in the moment object.
 export const momentToDate = (momentObj) => {
-    let formattedDate = momentObj.format("YYYY-MM-DDTHH:mm:ss");
+  let formattedDate = momentObj.format("YYYY-MM-DDTHH:mm:ss");
 
-    // Safari always takes the timezone from the local machine, even
-    // when the date is created as above. I'm changing the orientation
-    // of the offset to cancel the timezone.
-    if (isSafari()) {
-        // We get the local timezone and insert it into the formatted date
-        // to make Safari think that that's the local time
-        const offset = moment().format('Z');
+  // Safari always takes the timezone from the local machine, even
+  // when the date is created as above. I'm changing the orientation
+  // of the offset to cancel the timezone.
+  if (isSafari()) {
+    // We get the local timezone and insert it into the formatted date
+    // to make Safari think that that's the local time
+    const offset = moment().format("Z");
 
-        formattedDate += offset;
-    }
+    formattedDate += offset;
+  }
 
-    return new Date(formattedDate);
-}
+  return new Date(formattedDate);
+};
--- Client/src/utils/helpers.js
+++ Client/src/utils/helpers.js
@@ -1,149 +1,162 @@
-import React, { Profiler } from 'react';
+import React, { Profiler } from "react";
 import moment from "moment";
-import timeFormats from './timeFormats';
+import timeFormats from "./timeFormats";
 
 export function linkify(text) {
-    if(typeof(text) === "undefined") return;
-    var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    return text.replace(urlRegex, function(url) {
-        return '<a href="'+url+'" target="_blank">' + url + '</a>';
-    });
+  if (typeof text === "undefined") return;
+  var urlRegex =
+    /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+  return text.replace(urlRegex, function (url) {
+    return '<a href="' + url + '" target="_blank">' + url + "</a>";
+  });
 }
 
 export function removeUrl(text) {
-    var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    return text.replace(urlRegex, function(url) {
-        return '';
-    });
+  var urlRegex =
+    /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+  return text.replace(urlRegex, function (url) {
+    return "";
+  });
 }
 
-export function getUrlFromText(text){
-    var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    return text.match(urlRegex);
+export function getUrlFromText(text) {
+  var urlRegex =
+    /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+  return text.match(urlRegex);
 }
 
 export function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-export function truncate(string, length){
-    if(typeof(string) === "undefined" || !string) return;
-    if (string.length > length)
-       return string.substring(0,length)+'...';
-    else
-       return string;
-};
-
-export function hashLink(text, onTagClick){
-    if(typeof(text) === "undefined") return;
-    return text.replace(/([@#])([a-z\d_]+)/ig, (function(_, marker, tag){
-        if (marker === "@")
-            return '<a href="https://twitter.com/'+tag+'" target="_blank">@' + tag + '</a>';
-
-        if(typeof onTagClick === 'function')
-            return '<a href="javascript:void(0)">#' + tag+'</a>';
-        
-             
-        return '<a href="https://twitter.com/hashtag/'+tag+'" target="_blank">#' + tag+'</a>';
-    }));
+export function truncate(string, length) {
+  if (typeof string === "undefined" || !string) return;
+  if (string.length > length) return string.substring(0, length) + "...";
+  else return string;
 }
 
-export function parseTextWithLinks(text, onTagClick = false){
-    return hashLink(linkify(text), onTagClick);
+export function hashLink(text, onTagClick) {
+  if (typeof text === "undefined") return;
+  return text.replace(/([@#])([a-z\d_]+)/gi, function (_, marker, tag) {
+    if (marker === "@")
+      return (
+        '<a href="https://twitter.com/' +
+        tag +
+        '" target="_blank">@' +
+        tag +
+        "</a>"
+      );
+
+    if (typeof onTagClick === "function")
+      return '<a href="javascript:void(0)">#' + tag + "</a>";
+
+    return (
+      '<a href="https://twitter.com/hashtag/' +
+      tag +
+      '" target="_blank">#' +
+      tag +
+      "</a>"
+    );
+  });
+}
+
+export function parseTextWithLinks(text, onTagClick = false) {
+  return hashLink(linkify(text), onTagClick);
 }
 
 export function chunk(array, size) {
-    if (!array) return [];
-    const firstChunk = array.slice(0, size); // create the first chunk of the given array
-    if (!firstChunk.length) {
-      return array; // this is the base case to terminal the recursive
-    }
-    return [firstChunk].concat(chunk(array.slice(size, array.length), size)); 
+  if (!array) return [];
+  const firstChunk = array.slice(0, size); // create the first chunk of the given array
+  if (!firstChunk.length) {
+    return array; // this is the base case to terminal the recursive
+  }
+  return [firstChunk].concat(chunk(array.slice(size, array.length), size));
 }
 
 export function isEmptyObject(obj) {
-    for(var key in obj) {
-        if(obj.hasOwnProperty(key))
-            return false;
-    }
-    return true;
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) return false;
+  }
+  return true;
 }
 
-export function toHumanTime(time){
-    return moment.utc(Date.now()).to(moment.utc(time, timeFormats));
+export function toHumanTime(time) {
+  return moment.utc(Date.now()).to(moment.utc(time, timeFormats));
 }
 
 export function isOwner(accessLevel) {
-    return accessLevel === 'owner';
+  return accessLevel === "owner";
 }
 
 export function isOwnerOrAdmin(accessLevel) {
-    return accessLevel === 'owner' || accessLevel === 'admin';
+  return accessLevel === "owner" || accessLevel === "admin";
 }
 
 export function filterFacebookProfiles(channels) {
-    return channels.filter(channel => {
-        if (channel.type === 'facebook') {
-            // From facebook we only want the pages
-            return channel.details.account_type !== 'profile';
-        }
+  return channels.filter((channel) => {
+    if (channel.type === "facebook") {
+      // From facebook we only want the pages
+      return channel.details.account_type !== "profile";
+    }
 
-        // We want to retrieve all the channels that are not
-        // facebook profiles
-        return true;
-    });
+    // We want to retrieve all the channels that are not
+    // facebook profiles
+    return true;
+  });
 }
 
 export function isSafari() {
-    return navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&
-        navigator.userAgent &&
-        navigator.userAgent.indexOf('CriOS') == -1 &&
-        navigator.userAgent.indexOf('FxiOS') == -1;
+  return (
+    navigator.vendor &&
+    navigator.vendor.indexOf("Apple") > -1 &&
+    navigator.userAgent &&
+    navigator.userAgent.indexOf("CriOS") == -1 &&
+    navigator.userAgent.indexOf("FxiOS") == -1
+  );
 }
 
-export function setCookie(name,value,days, path = '/') {
-    var expires = '';
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = `; expires=${date.toUTCString()}`;
-    }
-    document.cookie = `${name}=${value || ''}${expires}; path=${path}`
+export function setCookie(name, value, days, path = "/") {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = `; expires=${date.toUTCString()}`;
+  }
+  document.cookie = `${name}=${value || ""}${expires}; path=${path}`;
 }
 
 export function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
 }
 
-export function eraseCookie(name) {   
-    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+export function eraseCookie(name) {
+  document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
 }
 
-export function capitalize(string)
-{
-    return string.charAt(0).toUpperCase() + string.slice(1);
+export function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export function getOffset( el ) {
-    var _x = 0;
-    var _y = 0;
-    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-        _x += el.offsetLeft - el.scrollLeft;
-        _y += el.offsetTop - el.scrollTop;
-        el = el.offsetParent;
-    }
-    return { top: _y, left: _x };
+export function getOffset(el) {
+  var _x = 0;
+  var _y = 0;
+  while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+    _x += el.offsetLeft - el.scrollLeft;
+    _y += el.offsetTop - el.scrollTop;
+    el = el.offsetParent;
+  }
+  return { top: _y, left: _x };
 }
